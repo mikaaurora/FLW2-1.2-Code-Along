92d0bb681c76528015372ff1b39bf2cae894e377
Mika Hughes <70777974+mikaaurora@users.noreply.github.com>
2022-10-03 12:09:22 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+//tile divs
+let tile1 = document.querySelector(".tile1");
+let tile2 = document.querySelector(".tile2");
+let tile3 = document.querySelector(".tile3");
+let tile4 = document.querySelector(".tile4");
+
+/*** task 1: declare a variable called fullName and 
+assign a string with your name or a favorite 
+celebrity.***/
+
+/*** task 2: declare a variable called favNum and 
+assign your favorite number as the value.***/
+
+/*** task 3: declare a variable called coolImg and 
+assign a string of an image address. Pick an image 
+you enjoy!***/
+
+/*** task 4: declare a variable called secondFavNum and 
+assign a number as the value.***/
+
+/*** task 5: declare a variable called sumOfNums and 
+assign the variable called favNum plus (added to) 
+the variable called secondFavNum.***/
+
+/*** task 6: UNCOMMENT THE CODE BELOW ONCE 
+ TASKS 1-5 ARE COMPLETE ***/
+
+/*
+tile1.innerHTML = fullName;
+tile2.innerHTML = favNum;
+let image = document.createElement("img");
+image.src = coolImg;
+tile3.appendChild(image);
+tile4.innerHTML = sumOfNums;
+*/
